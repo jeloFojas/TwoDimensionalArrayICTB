1c42dd50f53e51b9591ea7982609253880a07c63
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-27 08:52:34 +0000

    commit

--- main.cs
+++ main.cs
@@ -2,6 +2,16 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+    string [,] arrayName = { {"LANOY","ICT B","FEMALE","DENVER"},
+                              {"CASAS","ICT B","LESBIAN","SIR JELO",},
+                              {"CATAPIA","ICT B","MALE","MONTEVERDE"},
+                              {"HANGA-AN","ICT B","FEMALE","ANDREI"},
+                              {"TORRES","ICT B","FEMALE","SULATRA"},
+                              {"ESCOBIDO","ICT B","BINALEWALA","SIR LUKE"},
+    };
+
+    Console.WriteLine(arrayName[3,3]);
+    
   }
 }
